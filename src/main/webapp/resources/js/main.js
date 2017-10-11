7ca2bb00504010b58e135e0e2a7681d17a5d93b8
Khemroat Loem <khemroat@gmail.com>
2017-10-10 20:30:11 -0700

    [updated]: category autocomplete script file

---
+++ src/main/webapp/resources/js/main.js
@@ -0,0 +1,21 @@
+"use strict;"
+
+$(function() {
+
+	var $txtCategoryName = $("form[id=Category] input#name");
+	// check if user in create category page
+	if ($txtCategoryName.length > 0) {
+		$.get("/onlinetest/admin/list/category", function(data) {
+			$("form[id=Category] input#name").autocomplete({
+				source : data,
+				response : function(event, ui) {
+					// ui.content is the array that's about to be sent to the response callback.
+					if (ui.content.length === 0) {
+						alert("The category can be added!");
+					}
+				}
+			})
+		});
+	}
+
+});
